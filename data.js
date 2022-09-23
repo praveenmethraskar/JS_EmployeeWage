a4da8b7bd181735e71dd00f2c21a52ca9950f3d4
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-23 18:20:43 +0530

    Employee attendance

---
+++ data.js
@@ -0,0 +1,50 @@
+//array, function, spread operator, map
+/*
+var data=["raj","teju","sima"];
+console.log(data);
+
+var data1 = new Array("raj","teju","sima")
+console.log(data1[data.length-1]);
+console.log(data.sort());
+console.log(data.reverse());
+
+function Hello(name){
+    console.log("hello "+name)
+}
+Hello("praveen");*/
+/*
+function Hello()
+{
+    let a="hello";
+    if(a=="hello")
+    {
+        var b="World";
+        console.log(a+" "+b);
+    }
+    console.log(a+" "+b);
+}
+Hello();
+
+function Sum(a,b,c)
+{
+    console.log(a+b+c);
+}
+var arr=[1,2,3];
+Sum(...arr);//spread operator
+console.log(...arr);*/
+/*
+let arr1=[1,2,3];
+let arr2=[4,5,6];
+//arr1=arr1.concat(arr2);
+arr1=[...arr1,...arr2];
+console.log(arr1);
+
+let ar=[1,2,3];
+let ar2=[...ar];
+console.log(ar2);
+ar2.push(4,5);
+console.log(ar2);
+console.log(ar);*/
+
+
+
