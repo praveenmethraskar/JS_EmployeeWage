07ab35eace830446a96ce1c45a6de3ece1e85032
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-23 18:54:27 +0530

    Refactoring the code to function

--- Employee.js
+++ Employee.js
@@ -14,7 +14,8 @@ else{
 
 //UC2 employee wage calculation
 
-
+//UC2 employee wage calculations
+/*
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -33,4 +34,35 @@ switch(empCheck) {
 }
 
 let empWage = empHrs*WAGE_PER_HOUR;
+
 console.log("Emp Wage: "+empWage);
+
+console.log("Emp Wage: "+empWage);
+*/
+
+//UC3 Refactoring the code
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+function getWorkingHours(empCheck){
+    switch(empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
+}
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage "+empWage);
