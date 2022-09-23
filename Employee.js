1a0d534572859e024d534bfa0170d59c0e103a9c
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-23 19:12:52 +0530

    MAx days of 20 or 160hrs

--- Employee.js
+++ Employee.js
@@ -5,7 +5,12 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-const NUM_OF_WORKING_DAYS=2;
+const MAX_HRS_IN_MONTH=100;
+const NUM_OF_WORKING_DAYS=10;
+
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+
 function getWorkingHours(empCheck){
     switch(empCheck) {
         case IS_PART_TIME:
@@ -19,6 +24,16 @@ function getWorkingHours(empCheck){
     }
 }
 
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC-Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp wage: "+empWage);
+
+/*
 let empHrs=0;
 for(let day = 0; day<NUM_OF_WORKING_DAYS;day++){
     let empCheck=Math.floor(Math.random()*10)%3;
@@ -26,7 +41,7 @@ for(let day = 0; day<NUM_OF_WORKING_DAYS;day++){
 }
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hrs :" +empHrs+ "Emp Wage "+empWage);    
-
+*/
 //UC1
 /*
 const IS_ABSENT=0;
